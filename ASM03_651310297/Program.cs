48196ab487661e636a577775717634ecce4b1cdc
Tonrkz <tonrak.5@hotmail.com>
2023-10-20 03:02:23 +0700

    Add project files.

---
+++ ASM03_651310297/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ASM03_651310297 {
+    internal class Program {
+        static void Main(string[] args) {
+            Sprites aSprite = new Sprites();
+            Console.SetCursorPosition(30, 0);
+            Console.WriteLine(aSprite.zombie);
+        }
+    }
+}
