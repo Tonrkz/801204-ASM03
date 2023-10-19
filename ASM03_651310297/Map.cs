d35ced1b16abe0888cf18301385795bb4909852e
Tonrkz <tonrak.5@hotmail.com>
2023-10-20 06:14:45 +0700

    Create Classes

---
+++ ASM03_651310297/Map.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASM03_651310297 {
+    public class Map {
+    }
+}
---
+++ ASM03_651310297/Users.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASM03_651310297 {
+    public abstract class Users {
+
+    }
+}
